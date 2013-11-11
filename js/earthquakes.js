fb40506f3600b2a0a05e6f484cc6682b9e7a2779
unknown <iguest@is-m440m27-13.ischool.uw.edu>
2013-11-10 22:24:05 -0800

    add files.

--- js/earthquakes.js
+++ js/earthquakes.js
@@ -5,6 +5,36 @@
     app token (pass as '$$app_token' query string param): Hwu90cjqyFghuAWQgannew7Oi
 */
 
+
+//function to call when document is ready
+$(function(){
+   getQuakes(); 
+ }); //doc ready
+
+
+ //getQuakes()
+//queries the server for the list of recent quakes
+//and plots them on a Google map
+function getQuakes() {
+	$.getJSON(gov.usgs.quakesUrl, function(quakes){
+    //quakes is an array of objects, each of which represents info about a quake
+    //see data returned from:
+    //https://soda.demo.socrata.com/resource/earthquakes.json?$$app_token=Hwu90cjqyFghuAWQgannew7Oi
+
+    //set our global variable to the current set of quakes
+    //so we can reference it later in another event
+    gov.usgs.quakes = quakes;
+    $('.message').html('Displaying ' + quakes.length + ' earthquakes');
+    gov.usgs.quakesMap = new google.maps.Map($('.map-container')[0], {
+    center: new google.maps.LatLng(0,0),        //centered on 0/0
+    zoom: 2,                                    //zoom level 2
+    mapTypeId: google.maps.MapTypeId.TERRAIN,   //terrain map
+    streetViewControl: false                    //no street view
+	});
+    addQuakeMarkers(quakes, gov.usgs.quakesMap)
+	}); //handle returned data function
+}
+            
 //create a global variable namespace based on usgs.gov
 //this is how JavaScript developers keep global variables
 //separate from one another when mixing code from different
